ff5103a3d71780e3a8fd9dbf0485f367addcbc1c
MMadejsza <maciej.madejsza@gmail.com>
2022-12-21 10:05:05 +0000

    Commenting

--- Slider_practicing/js/index.js
+++ Slider_practicing/js/index.js
@@ -1,16 +1,21 @@
+//- catching DOM elements
 const imagesContainer = document.querySelector('.slider__images-container');
-let imagesContainerWidth;
 const image1 = document.querySelector('.slider__image-container--first img');
 const image2 = document.querySelector('.slider__image-container--second img');
-let dragging = 0;
-let imagesContainerLeftOffset;
 const img1ContainerEl = document.querySelector('.slider__image-container--first');
 const img2ContainerEl = document.querySelector('.slider__image-container--second');
 const handleEl = document.querySelector('.slider__handle');
 const dividerEl = document.querySelector('.slider__divider');
+//- global variables to define on action
+let imagesContainerWidth;
+let imagesContainerLeftOffset;
+//- flag to point dragging
+let dragging = 0;
 
 function getValidatedInnerOffset(distanceFromWindowLeft) {
+	//- get cursor position starting from it's left:
 	const innerMouseOffset = distanceFromWindowLeft - imagesContainerLeftOffset;
+	//- return only values within the element - no bigger then width or it's initial point:
 	if (innerMouseOffset < 0) {
 		return 0;
 	} else if (innerMouseOffset > imagesContainerWidth) {
@@ -20,11 +25,14 @@ function getValidatedInnerOffset(distanceFromWindowLeft) {
 	}
 }
 
-function moveBy(distanceFromWindowLeft) {
+function moveBasedOn(distanceFromWindowLeft) {
+	//- first validate what you've received:
 	const validatedInnerElementOffset = getValidatedInnerOffset(distanceFromWindowLeft);
-	const innerElOffsetPercentage = (validatedInnerElementOffset / imagesContainerWidth) * 100; // now truly screen size independent
+	//- in what percent is the cursor?
+	const innerElOffsetPercentage = (validatedInnerElementOffset / imagesContainerWidth) * 100; //- now it is truly screen size independent
+	//- apply received values:
 	dividerEl.style.left = `${innerElOffsetPercentage}%`;
-	img2ContainerEl.style.width = `${innerElOffsetPercentage}%`;
+	img2ContainerEl.style.width = `${innerElOffsetPercentage}%`; //- overlaying container - to let img adjust to it
 }
 
 function initEvents() {
@@ -33,26 +41,32 @@ function initEvents() {
 	handleEl.addEventListener('touchstart', () => (dragging = !dragging));
 	handleEl.addEventListener('touchend', () => (dragging = !dragging));
 	window.addEventListener('mousemove', (event) => {
+		//- When dragging:
 		if (dragging) {
 			let distanceFromWindowLeft = event.clientX;
-			moveBy(distanceFromWindowLeft);
+			//- pass cursor position to next function:
+			moveBasedOn(distanceFromWindowLeft);
 		}
 	});
 	window.addEventListener('touchmove', (event) => {
 		if (dragging) {
 			let distanceFromWindowLeft = event.touches[0].clientX; // first touch
-			moveBy(distanceFromWindowLeft);
+			moveBasedOn(distanceFromWindowLeft);
 		}
 	});
 }
 
+//- get up to date values:
 const adjustImgSize = function () {
-	imagesContainerWidth = imagesContainer.offsetWidth;
-	imagesContainerLeftOffset = imagesContainer.offsetLeft;
+	imagesContainerWidth = imagesContainer.offsetWidth; // = container width
+	imagesContainerLeftOffset = imagesContainer.offsetLeft; // = distance from window left to the container
 	image1.style.width = imagesContainerWidth + 'px';
 	image2.style.width = `${imagesContainerWidth}px`;
 };
 
+//- update coordinates on resizing:
 window.addEventListener('resize', adjustImgSize);
+//- get coordinates on start:
 adjustImgSize();
+//
 initEvents();
